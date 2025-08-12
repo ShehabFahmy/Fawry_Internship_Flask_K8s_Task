dcfe98e947650c76c71f0ef905c4b8f7d9cc08f2
ShehabFahmy <shehabmostafa2323@gmail.com>
2025-08-12 18:51:27 +0300

    Minor K8s Manifests Fixes

--- k8s/secret.sh
+++
@@ -1,4 +0,0 @@
-kubectl create secret docker-registry dockerhub-secret \
-  --docker-username=<YOUR_DOCKERHUB_USERNAME> \
-  --docker-password=<YOUR_DOCKERHUB_PASSWORD> \
-  --docker-email=<YOUR_EMAIL>
